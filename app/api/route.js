e273c28f0ea845f63289f21f4622e920d6a2f536
Lev Andreev <duffyduck451@gmail.com>
2023-06-02 19:28:07 +0200

    update ger request, fix bug

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
+import xml2js from "xml2js";
 
 export async function GET() {
-  const body = await request.json();
-  console.log(body);
-
   const soapRequest =
     '<?xml version="1.0" encoding="utf-8"?>\
     <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
@@ -25,37 +23,42 @@ export async function GET() {
         },
       }
     );
-
+    let gridResult = null;
     const responseData = response.data;
     // console.log(responseData);
-  } catch (error) {
-    console.error(error);
-  }
-  if (response.status === 200) {
-    console.log(2);
-    // Parse the SOAP response
-    xml2js.parseString(responseData, (err, result) => {
-      if (err) {
-        console.error(err);
-        console.log(3);
-        return  NextResponse.json({ Error: "Something went wrong"});
-      } else {
-        const dataItems = result['soap:Envelope']['soap:Body'][0]['AktualniSystemovaOdchylkaCRResponse'][0]['AktualniSystemovaOdchylkaCRResult'][0]['root'][0]['data'][0]['item'];
-        console.log(4);
-        // Extract the date and value from each item
-        const itemList = dataItems.map(item => ({
-          date: item.$.date,
-          value: item.$.value1
-        }));
-        console.log(5);
-        const lastElem = itemList[itemList.length - 1]
-        const gridResult = lastElem > 0 ? 1 : 0
-        return  NextResponse.json({ gridResult });
-      }
-    });
-  }
+    if (response.status === 200) {
+      console.log(2);
+      // Parse the SOAP response
+      xml2js.parseString(responseData, (err, result) => {
+        if (err) {
+          console.error(err);
+          console.log(3);
+          return NextResponse.json({ Error: "Something went wrong" });
+        } else {
+          const dataItems =
+            result["soap:Envelope"]["soap:Body"][0][
+              "AktualniSystemovaOdchylkaCRResponse"
+            ][0]["AktualniSystemovaOdchylkaCRResult"][0]["root"][0]["data"][0][
+              "item"
+            ];
+          console.log(4);
+          // Extract the date and value from each item
+          const itemList = dataItems.map((item) => ({
+            date: item.$.date,
+            value: item.$.value1,
+          }));
+          console.log(5);
+          const lastElem = itemList[itemList.length - 1];
+          gridResult = lastElem > 0 ? 1 : 0;
+        }
+      });
+    }
+    return NextResponse.json({ result: gridResult });
 
-  return NextResponse.json({ data });
+    // return NextResponse.json({ data });
+  } catch (error) {
+    return NextResponse.json({ Error: "Something went wrong" });
+  }
 }
 
 export async function POST(request) {
