572d83c53b234477ff314e820010e4bbe50a2550
Lev Andreev <duffyduck451@gmail.com>
2023-06-03 12:06:59 +0200

    update logic on api for hardware device to use - use prod server and after timeout test server of ceps

--- app/api/route.js
+++ app/api/route.js
@@ -2,18 +2,91 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 import xml2js from "xml2js";
 
+// export async function GET() {
+//   const soapRequest =
+//     '<?xml version="1.0" encoding="utf-8"?>\
+//     <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
+//         <soap12:Body>\
+//             <AktualniSystemovaOdchylkaCR xmlns="https://www.ceps.cz/CepsData/">\
+//                 <dateFrom>2023-06-01</dateFrom>\
+//                 <dateTo>2024-06-03</dateTo>\
+//             </AktualniSystemovaOdchylkaCR>\
+//         </soap12:Body>\
+//     </soap12:Envelope>';
+//   try {
+//     const response = await axios.post(
+//       "https://vip-test-service-00-azapp.azurewebsites.net/_layouts/CepsData.asmx",
+//       soapRequest,
+//       {
+//         headers: {
+//           "Content-Type": "application/soap+xml; charset=utf-8",
+//         },
+//       }
+//     );
+//     let gridResult = null;
+//     let lastElem = null;
+//     const responseData = response.data;
+//     if (response.status === 200) {
+//       // Parse the SOAP response
+//       xml2js.parseString(responseData, (err, result) => {
+//         if (err) {
+//           console.error(err);
+//           return NextResponse.json({ Error: "Something went wrong" });
+//         } else {
+//           const dataItems =
+//             result["soap:Envelope"]["soap:Body"][0][
+//               "AktualniSystemovaOdchylkaCRResponse"
+//             ][0]["AktualniSystemovaOdchylkaCRResult"][0]["root"][0]["data"][0][
+//               "item"
+//             ];
+//           // Extract the date and value from each item
+//           const itemList = dataItems.map((item) => ({
+//             date: item.$.date,
+//             value: item.$.value1,
+//           }));
+//           lastElem = itemList[itemList.length - 1];
+//           gridResult = parseInt(lastElem.value) > 0 ? 1 : 0;
+//         }
+//       });
+//     }
+//     return NextResponse.json({ result: gridResult, value: lastElem });
+
+//     // return NextResponse.json({ data });
+//   } catch (error) {
+//     return NextResponse.json({ Error: "Something went wrong" });
+//   }
+// }
 export async function GET() {
-  const soapRequest =
-    '<?xml version="1.0" encoding="utf-8"?>\
-    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
-        <soap12:Body>\
-            <AktualniSystemovaOdchylkaCR xmlns="https://www.ceps.cz/CepsData/">\
-                <dateFrom>2023-06-01</dateFrom>\
-                <dateTo>2024-06-03</dateTo>\
-            </AktualniSystemovaOdchylkaCR>\
-        </soap12:Body>\
-    </soap12:Envelope>';
+  const now = new Date();
+  // const formattedDateNow = now.toISOString().slice(0, 19) + "+02:00"; // Add the desired timezone offset
+  const formattedDateNow = "2027";
+  const fiveHoursAgo = new Date(now.getTime() - 5 * 60 * 60 * 1000);
+
+  // Convert to the desired format
+  const formattedDate = fiveHoursAgo.toISOString().slice(0, 19);
+  const timezoneOffset = fiveHoursAgo.getTimezoneOffset();
+  const timezoneOffsetFormatted =
+    (timezoneOffset < 0 ? "+" : "-") +
+    Math.abs(Math.floor(timezoneOffset / 60))
+      .toString()
+      .padStart(2, "0") +
+    ":" +
+    Math.abs(timezoneOffset % 60)
+      .toString()
+      .padStart(2, "0");
+
+  const fiveHoursAgoFormatted = formattedDate + timezoneOffsetFormatted;
+  const soapRequest = `<?xml version="1.0" encoding="utf-8"?>\
+      <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
+          <soap12:Body>\
+              <AktualniSystemovaOdchylkaCR xmlns="https://www.ceps.cz/CepsData/">\
+                  <dateFrom>${fiveHoursAgoFormatted}</dateFrom>\
+                  <dateTo>${formattedDateNow}</dateTo>\
+              </AktualniSystemovaOdchylkaCR>\
+          </soap12:Body>\
+      </soap12:Envelope>`;
   try {
+    let itemList = [];
     const response = await axios.post(
       "https://www.ceps.cz/_layouts/CepsData.asmx",
       soapRequest,
@@ -21,18 +94,87 @@ export async function GET() {
         headers: {
           "Content-Type": "application/soap+xml; charset=utf-8",
         },
+        timeout: 3000,
       }
     );
-    let gridResult = null;
-    let lastElem = null;
     const responseData = response.data;
+
     if (response.status === 200) {
       // Parse the SOAP response
       xml2js.parseString(responseData, (err, result) => {
-        if (err) {
-          console.error(err);
-          return NextResponse.json({ Error: "Something went wrong" });
-        } else {
+        const dataItems =
+          result["soap:Envelope"]["soap:Body"][0][
+            "AktualniSystemovaOdchylkaCRResponse"
+          ][0]["AktualniSystemovaOdchylkaCRResult"][0]["root"][0]["data"][0][
+            "item"
+          ];
+        // Extract the date and value from each item
+        itemList = dataItems.map((item) => ({
+          date: new Date(item.$.date).toLocaleString("en-GB", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }),
+          value: item.$.value1,
+        }));
+      });
+    }
+    // return NextResponse.json({ result: itemList, value: null });
+    return NextResponse.json({
+      // parseInt(lastElem.value) > 0 ? 1 : 0;
+      result: parseInt(itemList[itemList.length - 1].value) > 0 ? 1 : 0,
+      value: itemList[itemList.length - 1].value,
+    });
+  } catch (error) {
+    try {
+      const now = new Date();
+      const formattedDateNow = now.toISOString().slice(0, 19) + "+02:00"; // Add the desired timezone offset
+
+      const fiveHoursAgo = new Date(now.getTime() - 5 * 60 * 60 * 1000);
+
+      // Convert to the desired format
+      const formattedDate = fiveHoursAgo.toISOString().slice(0, 19);
+      const timezoneOffset = fiveHoursAgo.getTimezoneOffset();
+      const timezoneOffsetFormatted =
+        (timezoneOffset < 0 ? "+" : "-") +
+        Math.abs(Math.floor(timezoneOffset / 60))
+          .toString()
+          .padStart(2, "0") +
+        ":" +
+        Math.abs(timezoneOffset % 60)
+          .toString()
+          .padStart(2, "0");
+
+      const fiveHoursAgoFormatted = formattedDate + timezoneOffsetFormatted;
+      const soapRequest = `<?xml version="1.0" encoding="utf-8"?>\
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
+                <soap12:Body>\
+                    <AktualniSystemovaOdchylkaCR xmlns="https://www.ceps.cz/CepsData/">\
+                        <dateFrom>${fiveHoursAgoFormatted}</dateFrom>\
+                        <dateTo>${formattedDateNow}</dateTo>\
+                    </AktualniSystemovaOdchylkaCR>\
+                </soap12:Body>\
+            </soap12:Envelope>`;
+
+      let itemList = [];
+      const response = await axios.post(
+        "https://vip-test-service-00-azapp.azurewebsites.net/_layouts/CepsData.asmx",
+        soapRequest,
+        {
+          headers: {
+            "Content-Type": "application/soap+xml; charset=utf-8",
+          },
+          timeout: 10000,
+        }
+      );
+      const responseData = response.data;
+
+      if (response.status === 200) {
+        // Parse the SOAP response
+        xml2js.parseString(responseData, (err, result) => {
           const dataItems =
             result["soap:Envelope"]["soap:Body"][0][
               "AktualniSystemovaOdchylkaCRResponse"
@@ -40,20 +182,26 @@ export async function GET() {
               "item"
             ];
           // Extract the date and value from each item
-          const itemList = dataItems.map((item) => ({
-            date: item.$.date,
+          itemList = dataItems.map((item) => ({
+            date: new Date(item.$.date).toLocaleString("en-GB", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            }),
             value: item.$.value1,
           }));
-          lastElem = itemList[itemList.length - 1];
-          gridResult = parseInt(lastElem.value) > 0 ? 1 : 0;
-        }
+        });
+      }
+      return NextResponse.json({
+        result: parseInt(itemList[itemList.length - 1].value) > 0 ? 1 : 0,
+        value: itemList[itemList.length - 1].value,
       });
+    } catch (error) {
+      return NextResponse.json({ Error: "Something went wrong" });
     }
-    return NextResponse.json({ result: gridResult, value: lastElem });
-
-    // return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ Error: "Something went wrong" });
   }
 }
 
